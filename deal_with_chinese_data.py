53193851709a49045c9e3b28010d124c0fa99c26
Andy <conanandy@163.com>
2018-03-18 20:12:14 +0800

    finish jieba

--- deal_with_chinese_data.py
+++ deal_with_chinese_data.py
@@ -1,5 +1,4 @@
-# coding=gbk
-f = open('../Data/tieba_comments.txt', 'rb')
+f = open('../tieba/temp_comments_clean.txt', 'r')
 dialogues = []
 D = f.readlines()
 for i, line in enumerate(D):
@@ -19,5 +18,30 @@ for i, line in enumerate(D):
                 break
     else:
         continue
+'''f = open('../tieba/temp_comments_clean_2.txt', 'r')
+ff = open('../tieba/temp_comments_clean_3.txt', 'w')
+dialogues = []
+D = f.readlines()
+j = 0
+flag = 0
+for i, line in enumerate(D):
+    if i % 2 == flag:
+        if i+2 < len(D) and D[i] == D[i+2]:
+            ff.write(line)
+        elif i+2 >= len(D):
+            ff.write(line)
+        else:
+            count = 1
+            while (i+2*count+2 < len(D) and D[i+2*count] != D[i+2*count+2] and count < 20):
+                count += 1
+            if count > 8:
+                print(i)
+                #break
+                flag = 1 - flag
+            else:
+                ff.write(line)
+    else:
+        ff.write(line)'''
+
 
 
